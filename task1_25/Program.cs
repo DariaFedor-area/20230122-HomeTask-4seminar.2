25a68b0edb5fcebb5ed53801c3d4ffd546f302f3
Daria <dorothyfedorova@gmail.com>
2023-01-22 19:43:08 +0300

    add solution of task25

--- task1_25/Program.cs
+++ task1_25/Program.cs
@@ -7,3 +7,21 @@
 возводит число А в натуральную степень В.
 3, 5 -> 243 (3 в пятой)
 2, 4 -> 16 */
+
+Console.Clear();
+Console.WriteLine("Возведем число А в степень числа В");
+Console.Write("внесите первое число: А = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("внесите второе число: В = ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int ToDegree (int A, int B)
+{
+    int result = 1;
+    for (int x = 1; x <= B; x++)    
+    {
+        result = result * A;
+    }
+    return result;
+}
+Console.WriteLine($"результат возведения А в степень В = {ToDegree(A,B)}");
