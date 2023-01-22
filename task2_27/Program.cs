65f5b4d462a848a0bda785507a29713b9857c037
Daria <dorothyfedorova@gmail.com>
2023-01-22 19:21:38 +0300

    add folders for each tasks

---
+++ task2_27/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+/* ЗАДАЧА_27: напишите пограмму, которая принимает на вход число им выдает сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
---
+++ task3_29/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+/* ЗАДАЧА 29: Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+1,2,5,7,19 -> [1,2,5,7,19]
+6,1,33 -> [6,1,33]
+*/
