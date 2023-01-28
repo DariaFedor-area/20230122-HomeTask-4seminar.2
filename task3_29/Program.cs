3af3288f8472ff20033448f0ee5cd173b3f47856
Daria <dorothyfedorova@gmail.com>
2023-01-28 14:35:12 +0300

    add solution of task29

--- task3_29/Program.cs
+++ task3_29/Program.cs
@@ -8,8 +8,21 @@
 */
 
 Console.Clear();
+Console.WriteLine("Давайте выведем массив на экран. Задайте его длину и диапазон чисел");
+Console.Write("Длина массива = ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Диапазон от = ");
+int minNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Диапазон до = ");
+int maxNum = Convert.ToInt32(Console.ReadLine());
 
-int [] CreatArray (int length, int minNum, int maxNum)
+int[] CreateArray(int length, int minNum, int maxNum)
 {
-    
+    int[] Array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Array[i] = new Random().Next(minNum, maxNum + 1);
+    }
+    return Array;
 }
+ Console.WriteLine($"массив : "+ string.Join(",", CreateArray(length, minNum, maxNum)));
